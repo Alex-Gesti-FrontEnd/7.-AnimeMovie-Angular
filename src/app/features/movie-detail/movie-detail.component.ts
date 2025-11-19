147a97f6094c1055486e617401644b651d71cff0
AlexGesti <alexgesti001@gmail.com>
2025-11-19 13:16:57 +0100

    feat: implementar navegación básica a la siguiente página

---
+++ src/app/features/movie-detail/movie-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, signal, effect } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from '../../core/services/movies.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-movie-detail',
+  imports: [],
+  templateUrl: './movie-detail.component.html',
+  styleUrl: './movie-detail.component.scss',
+})
+export class MovieDetailComponent {
+  private readonly movieService = inject(MovieService);
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  private readonly id = toSignal(this.route.paramMap, { initialValue: null });
+
+  movie = signal<any>(null);
+  cast = signal<any[]>([]);
+
+  constructor() {
+    effect(() => {
+      const param = this.id();
+      if (!param) return;
+
+      const movieId = Number(param.get('id'));
+
+      this.movieService.getMovieDetail(movieId).subscribe((m) => this.movie.set(m));
+      this.movieService.getMovieCast(movieId).subscribe((c) => this.cast.set(c.cast));
+    });
+  }
+
+  protected goToMenu(): void {
+    this.router.navigate(['/']);
+  }
+}
